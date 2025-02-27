66e7ecea616412b9ee300f5ceb3063af765c3ac4
Thiago <thiagowendtpercyak@outlook.com>
2025-02-27 15:37:51 -0300

    models e views

--- estacionamento/base/views.py
+++ estacionamento/base/views.py
@@ -1,3 +1,19 @@
+from urllib import response
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
+from django.db.models import query
+from .models import Movimentacao
+
+def home(request):
+
+    movimentacao = Movimentacao
+    query = Movimentacao.objects.all()
+    context = {
+        'movimentacao' : movimentacao,
+        'query' : query,
+    }
+
+    return render(request, 'home.html')
 
 # Create your views here.
